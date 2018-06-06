3f28dbb4be67d0b2dd69dee2e904215842882372
David Bailey <dbailey@ucar.edu>
2018-06-06 10:56:30 -0600

    Update html style

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("cicedoc  documentation");
+        $("li.nav-item-0 a").text("cicedoc CICE 5.1.2 documentation");
     }
 });
